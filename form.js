c35406824a5a13cdbe2318e41b574d40a4c49668
gauravbestpeer <gapal@bestpeers.com>
2022-11-07 10:52:26 +0530

    updates on form & table data

--- form.js
+++ form.js
@@ -1,42 +1,49 @@
-function mySubmit(event){
-    console.log(event.target.value,"event");
+function mySubmit(event) {
     event.preventDefault()
-    let arr1 = []
-    var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
-    var regPhone=/^\d{10}$/; 
-    let Name = document.getElementById("Name").value;
-    let Email = document.getElementById("Email").value;
-    //  let Gender = document.querySelectorAll("input[type=radio][name=Gender]:checked")?.value
-    let Number = document.getElementById("phone").value;
-    if(Name === ""){
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+    var regPhone = /^\d{10}$/;
+    let name = document.getElementById("Name").value;
+    let email = document.getElementById("Email").value;
+    let number = document.getElementById("phone").value;
+    var male = document.getElementById("male").checked;
+    var female = document.getElementById("female").checked;
+    var gender = '';
+    var allData = document.getElementById("demo")
+    if (male) {
+        gender = document.getElementById("male").value
+    }
+    if (female) {
+        gender = document.getElementById("female").value
+    }
+    if (name === "") {
         document.getElementById("error").innerHTML = "please enter your name"
     }
-    else if(Email === "" || !regEmail.test(Email)){
+    else if (email === "") {
         document.getElementById("error").innerHTML = "please enter your email"
     }
-    // else if(Gender?.value === undefined ){
-    //     document.getElementById("error").innerHTML = "please enter your gender"
-    //    console.log(Gender);
-    // }
-    else if(Number === "" || !regPhone.test(Number)){
+    else if (!regEmail.test(email)) {
+        document.getElementById("error").innerHTML = "please enter your valid email"
+    }
+    else if (gender === "") {
+        document.getElementById("error").innerHTML = "please enter your gender"
+    }
+    else if (number === "") {
         document.getElementById("error").innerHTML = "please enter your Number"
     }
-    else{        
-    console.log(Name,Email,Number);
-    //   let arr = {
-    //         name:Name,
-    //         email:Email,
-    //         number:Number
-    //     }
-    //   arr1 = [...arr1,...arr]
-    // console.log(Name,Email,Number);
-    document.getElementById("Name").value = ""
-    document.getElementById("Email").value=""
-    // document.querySelector('input[name="Gender"]:checked').checked = false
-    document.getElementById("phone").value= ""
+    else if (!regPhone.test(number)) {
+        document.getElementById("error").innerHTML = "please enter your valid Number"
     }
-    
- 
-   
+    else {
+        console.log(name, email, number, gender);
+        let a = 0;
+        allData.innerHTML += `<tr id="${a++}"><td>${name}</td><td>${number}</td><td>${email}</td><td>${gender}</td><td></td></tr>`
+        document.getElementById("Name").value = ""
+        document.getElementById("Email").value = ""
+        document.getElementById("phone").value = ""
+        document.getElementById("error").innerHTML = ""
+        document.getElementById("male").checked = false
+        document.getElementById("female").checked = false
+        gender == ""
+    }
 
 }
